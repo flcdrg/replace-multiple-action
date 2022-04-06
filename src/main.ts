c0e09b6688947ef3a920616a9c9b5e6e76ab5780
David Gardiner <david@gardiner.net.au>
2022-04-06 22:20:01 +0930

    Replace implementation (#8)
    * Replace implementation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import * as core from '@actions/core';
-//import { promises as fs } from 'fs';
+import glob from 'glob';
+import { promises as fs } from 'fs';
+
+export type IReplacements = {
+  find: string;
+  replace: string;
+}[];
 
 async function run(): Promise<void> {
   try {
@@ -7,8 +13,31 @@ async function run(): Promise<void> {
     const files: string = core.getInput('files', { required: true });
     const find: string = core.getInput('find', { required: true });
 
+    const encoding: BufferEncoding = core.getInput(
+      'encoding'
+    ) as BufferEncoding;
+
     core.info(files);
     core.info(find);
+
+    const findData: IReplacements = JSON.parse(find);
+
+    // options is optional
+    glob('**/*.md', {}, async (er, matchedFiles) => {
+      for (const file of matchedFiles) {
+        const originalContent = await fs.readFile(file, encoding);
+        let content = originalContent;
+
+        for (const pair of findData) {
+          content = content.replace(pair.find, pair.replace);
+        }
+
+        if (originalContent !== content) {
+          await fs.writeFile(file, content, encoding);
+          core.info(`Updating ${file}`);
+        }
+      }
+    });
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
